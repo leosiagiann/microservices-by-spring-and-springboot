42335c7c6ee4b2d35b12bcfbd917736a1d301c66
Leonardo Siagian <if319017@students.del.ac.id>
2024-03-10 12:29:55 +0700

    feat: Update feign call method

---
+++ transaction-service/src/main/java/com/springboot/transaction/invoker/ProductFeignInvoker.java
@@ -0,0 +1,39 @@
+package com.springboot.transaction.invoker;
+
+import com.springboot.product.feign.ProductServiceFeignClient;
+import com.springboot.product.feign.dto.response.ProductResponseDTO;
+import feign.FeignException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+/**
+ * @Author: Leonardo Siagian
+ * Created on 10/03/2024
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ProductFeignInvoker {
+    private final ProductServiceFeignClient productServiceFeignClient;
+
+    public ProductResponseDTO findProductById(Long productId) {
+        try {
+            final ResponseEntity<ProductResponseDTO> response = productServiceFeignClient.findProductById(productId);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                final ProductResponseDTO productResponse = response.getBody();
+                if (Objects.nonNull(productResponse)) {
+                    return response.getBody();
+                }
+            }
+            log.error("ProductServiceFeignClient::findProductById response error, details {}", response);
+        } catch (Exception e) {
+            log.error("ProductServiceFeignClient::findProductById feign error, details");
+            throw e;
+        }
+        return new ProductResponseDTO();
+    }
+}
--- transaction-service/src/main/java/com/springboot/transaction/service/TransactionService.java
+++ transaction-service/src/main/java/com/springboot/transaction/service/TransactionService.java
@@ -1,20 +1,21 @@
 package com.springboot.transaction.service;
 
-import com.springboot.product.feign.ProductServiceFeignClient;
 import com.springboot.product.feign.dto.response.ProductResponseDTO;
-import com.springboot.transaction.configuration.MapperConfig;
 import com.springboot.transaction.dto.request.TransactionRequestDTO;
 import com.springboot.transaction.dto.response.TransactionResponseDTO;
 import com.springboot.transaction.entity.Transaction;
+import com.springboot.transaction.invoker.ProductFeignInvoker;
 import com.springboot.transaction.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang.math.NumberUtils;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 /**
@@ -25,28 +26,32 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class TransactionService {
     private final TransactionRepository transactionRepository;
-    private final ProductServiceFeignClient productServiceFeignClient;
+    private final ProductFeignInvoker productFeignInvoker;
     private final ModelMapper modelMapper;
+    private final TransactionTemplate transactionTemplate;
 
     public TransactionResponseDTO createTransaction(TransactionRequestDTO transactionRequestDTO) {
-        Transaction transaction = new Transaction();
-        transaction.setUserId(transactionRequestDTO.getUserId());
-        transaction.setProductId(transactionRequestDTO.getProductId());
-        transaction.setQuantity(transactionRequestDTO.getQuantity());
-        transaction.setPaymentMethod(transactionRequestDTO.getPaymentMethod());
-        transaction.setShippingAddress(transactionRequestDTO.getShippingAddress());
-        String name = productServiceFeignClient.findProductByName("canon").getBody().get(0).getName();
-        ProductResponseDTO productResponseDTO = productServiceFeignClient.findProductById(1L).getBody();
-        transaction.setTotalPrice(0D);
-        if (Objects.nonNull(productResponseDTO)) {
-            transaction.setTotalPrice(productResponseDTO.getPrice() * transaction.getQuantity());
-        }
-        transaction.setTransactionDate(LocalDateTime.now());
-        transaction.setStatus("PROCESSING");
-        transaction.setTrackingNumber("PROCESSING");
-        transaction = transactionRepository.save(transaction);
-        TransactionResponseDTO responseDTO = modelMapper.map(transaction, TransactionResponseDTO.class);
-        return responseDTO;
+        AtomicReference<TransactionResponseDTO> atomicReference = new AtomicReference<TransactionResponseDTO>();
+        transactionTemplate.execute(status -> {
+            Transaction transaction = new Transaction();
+            transaction.setUserId(transactionRequestDTO.getUserId());
+            transaction.setProductId(transactionRequestDTO.getProductId());
+            transaction.setQuantity(transactionRequestDTO.getQuantity());
+            transaction.setPaymentMethod(transactionRequestDTO.getPaymentMethod());
+            transaction.setShippingAddress(transactionRequestDTO.getShippingAddress());
+            ProductResponseDTO productResponseDTO = productFeignInvoker.findProductById(transaction.getProductId());
+            transaction.setTotalPrice(NumberUtils.DOUBLE_ZERO);
+            if (Objects.nonNull(productResponseDTO.getPrice())) {
+                transaction.setTotalPrice(productResponseDTO.getPrice() * transaction.getQuantity());
+            }
+            transaction.setTransactionDate(LocalDateTime.now());
+            transaction.setStatus("PROCESSING");
+            transaction.setTrackingNumber("PROCESSING");
+            transaction = transactionRepository.save(transaction);
+            atomicReference.set(modelMapper.map(transaction, TransactionResponseDTO.class));
+            return Boolean.TRUE;
+        });
+        return atomicReference.get();
     }
 
 
